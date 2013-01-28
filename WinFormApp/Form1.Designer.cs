b36c8fa4b10756f8df30cfafbc5786f13d884e69
adrianojlt <adrianojlt@gmail.com>
2013-01-28 23:19:39 +0000

    first commmit

---
+++ WinFormApp/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WinFormApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("root");
+            this.tree = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tree
+            // 
+            this.tree.Location = new System.Drawing.Point(12, 12);
+            this.tree.Name = "tree";
+            treeNode1.Name = "root";
+            treeNode1.Text = "root";
+            this.tree.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.tree.Size = new System.Drawing.Size(159, 345);
+            this.tree.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 369);
+            this.Controls.Add(this.tree);
+            this.Name = "MainForm";
+            this.Text = "WinFormApp";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tree;
+    }
+}
+
---
+++ WinFormApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            myInit();
+        }
+
+        public void myInit()
+        {
+            //this.Text = "Calculator";
+            //this.Size = new Size(200, 225);
+        }
+    }
+}
